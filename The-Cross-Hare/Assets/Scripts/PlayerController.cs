a1305f4595bbd2157ccaf71bcf29f1bdd7b09e2f
RubyAndreatta <Ruby.Andreatta@gmail.com>
2022-05-18 12:35:07 +1000

    Player moves

--- The-Cross-Hare/Assets/Scripts/PlayerController.cs
+++ The-Cross-Hare/Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+//using UnityEngine.InputSystem;
 using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
@@ -9,12 +9,71 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
+    private Vector2 movementInput;
+    private Vector3 direction;
+
+    bool hasMoved;
+
+
     void Update()
     {
-        
+        if (movementInput.x == 0)
+        {
+            hasMoved = false;
+        }
+        else if (movementInput.x != 0 && !hasMoved)
+        {
+            hasMoved = true;
+            //GetMovementDirection();
+        }
+
+
+
+       // (if enter, move forward. trying to go back causes red effect and no movement)
+       
+    }
+
+
+
+    public void GetMovementDirection()
+    {
+        if (movementInput.x < 0)
+        {
+            if (movementInput.y > 0)
+            {
+                direction = new Vector3(-0.5f, 0.5f);
+            }
+            else if (movementInput.y < 0)
+            {
+                direction = new Vector3(-0.5f, -0.5f);
+            }
+            else
+            {
+                direction = new Vector3(-1, 0, 0);
+            }
+            transform.position += direction;
+        }
+
+        else if (movementInput.x > 0)
+        {
+            if (movementInput.y > 0)
+            {
+                direction = new Vector3(0.5f, 0.5f);
+            }
+            else if (movementInput.y < 0)
+            {
+                direction = new Vector3(0.5f, -0.5f);
+            }
+            else
+            {
+                direction = new Vector3(-1, 0, 0);
+            }
+            transform.position += direction;
+        }
+
+        Debug.Log("e");
     }
 }
