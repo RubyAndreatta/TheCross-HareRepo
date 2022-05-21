8f8675c2ccabf3f584172081297b6406e032a659
RubyAndreatta <Ruby.Andreatta@gmail.com>
2022-05-21 15:57:27 +1000

    Collision now works, yet not linked to narrative

---
+++ The-Cross-Hare/Assets/Scripts/ReactionTile.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ReactionTile : MonoBehaviour
+{
+
+    public void Update()
+    {
+       
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("hit");
+    }
+
+}
+    
+
