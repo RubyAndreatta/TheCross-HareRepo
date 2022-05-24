a116f2ccc342bcc591cfff79da40a11380107d73
RubyAndreatta <Ruby.Andreatta@gmail.com>
2022-05-24 11:01:50 +1000

    JumpingWorks

---
+++ The-Cross-Hare/Assets/Scripts/CallingScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class CallingScript : MonoBehaviour
+{
+    public Fungus.Flowchart callingFlowchart;
+    public Collider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (callingFlowchart.FindBlock("MoveToCries").IsExecuting())
+        {
+            // this.GetComponent<PlayerMovement>().enabled = false;
+
+            // callingFlowchart.ExecuteBlock("Polecatscries");
+           // Debug.Log("block working");
+            transform.position = col.transform.position;
+        }
+    }
+
+
+}
