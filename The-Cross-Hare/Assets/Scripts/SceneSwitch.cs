b7c8accaf94ce2f2319c9f99b89b808c46d976fb
RubyAndreatta <Ruby.Andreatta@gmail.com>
2022-05-26 18:29:26 +1000

    Cleaning scripts

--- The-Cross-Hare/Assets/Scripts/SceneSwitch.cs
+++ The-Cross-Hare/Assets/Scripts/SceneSwitch.cs
@@ -9,18 +9,6 @@ public class SceneSwitch : MonoBehaviour
     {
         SceneManager.LoadScene(SampleScene);
     }
-        
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     
 
